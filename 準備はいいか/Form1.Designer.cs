62a781f37bbdfdaf250cd3276fa6849d424e736f
Yurina-Shigemoto <kawaisora522@gmail.com>
2025-06-19 15:01:24 +0900

    Add files via upload

---
+++ 準備はいいか/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace 準備はいいか
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblTime;
+
+        private void InitializeComponent()
+        {
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnStartStop.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnStartStop.ForeColor = System.Drawing.Color.Black;
+            this.btnStartStop.Location = new System.Drawing.Point(35, 194);
+            this.btnStartStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(100, 61);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = false;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnReset.ForeColor = System.Drawing.Color.Black;
+            this.btnReset.Location = new System.Drawing.Point(233, 194);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 61);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblTime.Location = new System.Drawing.Point(88, 71);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(192, 50);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(379, 301);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStartStop);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "俺は出来てる";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ 準備はいいか/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace 準備はいいか
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopwatch = new Stopwatch();
+        private Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000; // 1秒ごとに更新
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = stopwatch.Elapsed.ToString(@"hh\:mm\:ss");
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (stopwatch.IsRunning)
+            {
+                stopwatch.Stop();
+                timer.Stop();
+                btnStartStop.Text = "Start";
+            }
+            else
+            {
+                stopwatch.Start();
+                timer.Start();
+                btnStartStop.Text = "Stop";
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+            lblTime.Text = "00:00:00";
+            btnStartStop.Text = "Start";
+        }
+    }
+}
